7f576cf49fc7a9dc1baf16d621203025e29f2452
Taehyung Kwon <katalogin88@gmail.com>
2023-10-02 01:38:00 +0100

    Add tech info to portfolio cards

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -45,6 +45,7 @@ export const Portfolio = () => {
                 <img src={data.img_src} alt="" />
                 <div className="content">
                   <p>{data.content}</p>
+                  <p>{data.additional_info}</p>
                   <div className="link">
                     <a href={data.site_link}>Demo</a>
                     <a href={data.code_link}>Github</a>
